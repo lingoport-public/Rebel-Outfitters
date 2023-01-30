2222b73c26532e36c746bdcb3009152eb74440e8
Olivier Libouban <oliboubanLingoport@users.noreply.github.com>
2023-01-30 07:45:59 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -167,7 +167,7 @@ var readCart = () => {
         for(let productAr of cartIds) {
             if(productAr[1] == 'droid') { //$NON-NLS-L$
                 let product = droidMap.get(parseInt(productAr[0]));
-                product.qty = parseInt(productAr[2]);
+                product.qty = Number.parseLocale(productAr[2]);
                 shoppingCart[productAr[0]] = product;
             }
             else {
