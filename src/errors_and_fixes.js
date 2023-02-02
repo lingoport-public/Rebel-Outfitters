f4ccd912b7384f617f14a1e48c67b09126a0cee7
Olivier Libouban <oliboubanLingoport@users.noreply.github.com>
2023-02-02 09:15:14 -0700

    Update errors_and_fixes.js

--- src/errors_and_fixes.js
+++ src/errors_and_fixes.js
@@ -26,12 +26,12 @@ String s2 = i18nUtil.getMessage("ID_IN_MOV_2");
 //String s3 = "Gathers and washes and is gone";
 String s3 = i18nUtil.getMessage("ID_GATH_WASH_3");
 String s4 = "A slow obscure metonymy of motion";
-String s5 = "Crumbling the inner barriers of the brain";
-String s6 = "But the crossed rock braces the hills";
-String s7 = "A steady quiet";
+String s5 = i18nUtil.getMessage("ID_CRM_4");
+String s6 = i18nUtil.getMessage("ID_CROSS_5");
+String s7 = i18nUtil.getMessage("ID_QUIET_6");
 
 //Hello, more comments
-String s8 = "bad thing";
+String s8 = "bad thing"; // $NON-NLS-L$
 
 /* Adding comments to see what 'fix' v. 'open' will do in Command Center */
 String s10 = i18n.getString("Hello Command Center");
