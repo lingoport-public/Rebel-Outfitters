c4d1a34d78ec24bbffd8344a287a7ca0f24dd182
Olivier Libouban <oliboubanLingoport@users.noreply.github.com>
2023-02-09 14:52:35 -0700

    Update errors_and_fixes.js

--- src/errors_and_fixes.js
+++ src/errors_and_fixes.js
@@ -19,9 +19,9 @@ require.config({
     }
 });
 
-// String s = "Beyond the steady rock the steady sea";
+String s = "Beyond the steady rock the steady sea";
 String s = i18nUtil.getMessage("ID_BEYON_1");
-// String s2 = "In movement more immovable than station";
+String s2 = "In movement more immovable than station";
 String s2 = i18nUtil.getMessage("ID_IN_MOV_2");
 //String s3 = "Gathers and washes and is gone";
 String s3 = i18nUtil.getMessage("ID_GATH_WASH_3");
