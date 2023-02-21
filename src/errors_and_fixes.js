5a8bd026dc900c6ce57325669623a36be26e353e
Olivier Libouban <oliboubanLingoport@users.noreply.github.com>
2023-02-21 12:40:58 -0700

    Update errors_and_fixes.js

--- src/errors_and_fixes.js
+++ src/errors_and_fixes.js
@@ -19,12 +19,12 @@ require.config({
     }
 });
 
-// String s = "Beyond the steady rock the steady sea";
-String s = i18nUtil.getMessage("ID_BEYON_1");
-// String s2 = "In movement more immovable than station";
-String s2 = i18nUtil.getMessage("ID_IN_MOV_2");
-//String s3 = "Gathers and washes and is gone";
-String s3 = i18nUtil.getMessage("ID_GATH_WASH_3");
+String s = "Beyond the steady rock the steady sea";
+//String s = i18nUtil.getMessage("ID_BEYON_1");
+String s2 = "In movement more immovable than station";
+//String s2 = i18nUtil.getMessage("ID_IN_MOV_2");
+String s3 = "Gathers and washes and is gone";
+//String s3 = i18nUtil.getMessage("ID_GATH_WASH_3");
 String s4 = "A slow obscure metonymy of motion";
 String s5 = i18nUtil.getMessage("ID_CRM_4");
 String s6 = i18nUtil.getMessage("ID_CROSS_5");
@@ -34,8 +34,9 @@ String s7 = i18nUtil.getMessage("ID_QUIET_6");
 String s8 = "bad thing"; // $NON-NLS-L$
 
 /* Adding comments to see what 'fix' v. 'open' will do in Command Center */
-String s10 = i18n.getString("Hello Command Center");
-String s11 = "Hello Command Center"; // $NON-NLS-L$
+// String s10 = i18n.getString("Hello Command Center");
+String s10 = "Hello Command Center";
+String s11 = "Command Center"; // $NON-NLS-L$
 
 ;
 
