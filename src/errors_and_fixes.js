e19c3dca9dfde1e620aad343a10491bfabdd00d1
Olivier Libouban <oliboubanLingoport@users.noreply.github.com>
2023-08-17 10:54:58 -0600

    Update errors_and_fixes.js

--- src/errors_and_fixes.js
+++ src/errors_and_fixes.js
@@ -19,6 +19,10 @@ require.config({
     }
 });
 
+//Hello, more comments
+String s18 = "Welcome to Command Center. Please enter your licensing information"; 
+String s20 = "Please configure the Notification Email in the Settings page."; 
+
 String s = i18nUtil.getMessage("ID_BEYON_1");
 String s2 = i18nUtil.getMessage("ID_IN_MOV_2");
 //String s2 = "In movement more immovable than station";
@@ -30,16 +34,11 @@ String s5 = i18nUtil.getMessage("ID_CRM_4");
 String s6 = i18nUtil.getMessage("ID_CROSS_5");
 String s7 = i18nUtil.getMessage("ID_QUIET_6");
 
-//Hello, more comments
-String s18 = "Welcome to Command Center. Please enter your licensing information"; 
-String s20 = "Please configure the Notification Email in the Settings page."; 
-
 /* Adding comments to see what 'fix' v. 'open' will do in Command Center */
 // String s10 = i18n.getString("Hello Command Center");
 String s10 = "Hello Command Center";
 String s11 = "Command Center"; // $NON-NLS-L$
 
-;
 
 File image = olivier.png;
 
